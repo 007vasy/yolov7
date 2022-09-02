8a00c6d1576bd6a0a793b71ede129e94aa1c089f
ben <12837887+007vasy@users.noreply.github.com>
2022-09-02 10:36:25 +1000

    poc work but runs out of mem

--- keypoint_check.py
+++ keypoint_check.py
@@ -7,8 +7,13 @@ import numpy as np
 from utils.datasets import letterbox
 from utils.general import non_max_suppression_kpt, check_img_size
 from utils.plots import output_to_keypoint, plot_skeleton_kpts
+from utils.torch_utils import select_device, load_classifier, time_synchronized, TracedModel
 
 def main():
+
+    source = '2'
+    imgsz  = 640
+
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     weigths = torch.load('yolov7-w6-pose.pt', map_location=device)
     model = weigths['model']
@@ -17,31 +22,37 @@ def main():
     if torch.cuda.is_available():
         model.half().to(device)
 
-    image = cv2.imread('./headshot.jpg')
-    image = letterbox(image, 960, stride=64, auto=True)[0]
-    image_ = image.copy()
-    image = transforms.ToTensor()(image)
-    image = torch.tensor(np.array([image.numpy()]))
+    stride = int(model.stride.max())  # model stride
+    imgsz = check_img_size(imgsz, s=stride)  # check img_size
 
-    if torch.cuda.is_available():
-        image = image.half().to(device)   
-    output, _ = model(image)
+    dataset = LoadStreams(source, img_size=imgsz, stride=stride)
 
-    if torch.cuda.is_available():
-        image = image.half().to(device)   
-    output, _ = model(image)
+    for path, image, im0s, vid_cap in dataset:
+        image = torch.from_numpy(image).to(device)
+        image = image.half()
+        image /= 255.0  # 0 - 255 to 0.0 - 1.0
+        if image.ndimension() == 3:
+            image = image.unsqueeze(0)
 
-    output = non_max_suppression_kpt(output, 0.25, 0.65, nc=model.yaml['nc'], nkpt=model.yaml['nkpt'], kpt_label=True)
-    with torch.no_grad():
-        output = output_to_keypoint(output)
-    nimg = image[0].permute(1, 2, 0) * 255
-    nimg = nimg.cpu().numpy().astype(np.uint8)
-    nimg = cv2.cvtColor(nimg, cv2.COLOR_RGB2BGR)
-    for idx in range(output.shape[0]):
-        plot_skeleton_kpts(nimg, output[idx, 7:].T, 3)
+        if torch.cuda.is_available():
+            image = image.half().to(device)
+        t1 = time_synchronized()
+        output, _ = model(image)
+        t2 = time_synchronized()
 
-    cv2.imshow('test', nimg)
-    cv2.waitKey(10000)
+
+        output = non_max_suppression_kpt(output, 0.25, 0.65, nc=model.yaml['nc'], nkpt=model.yaml['nkpt'], kpt_label=True)
+        t3 = time_synchronized()
+        with torch.no_grad():
+            output = output_to_keypoint(output)
+        nimg = image[0].permute(1, 2, 0) * 255
+        nimg = nimg.cpu().numpy().astype(np.uint8)
+        nimg = cv2.cvtColor(nimg, cv2.COLOR_RGB2BGR)
+        for idx in range(output.shape[0]):
+            plot_skeleton_kpts(nimg, output[idx, 7:].T, 3)
+
+        cv2.imshow('test', nimg)
+        cv2.waitKey(1)
 
 if __name__ == '__main__':
     main()
